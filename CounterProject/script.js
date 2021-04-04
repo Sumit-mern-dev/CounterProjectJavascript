c0722759fd48d737ebd3395b53f219351a7b9565
Sumit Das <sumitdastechweb@gmail.com>
2021-04-04 21:04:11 +0530

    Final Submission of the counter project

---
+++ CounterProject/script.js
@@ -0,0 +1,19 @@
+ var num=document.querySelector(".counter");
+ var followersText=document.querySelector(".followers");
+ let i=1;
+
+ setInterval(()=>{
+     
+   if(i<1000)
+   {
+       i++;
+       num.innerText=i;
+       
+   }
+   else
+   {
+       followersText.innerText="Followers you have,Congrast!!"
+   }
+
+
+ },10);
